40c4733ec925dd6586f77ae8ffd7ee62399f36f4
Yahya Şamil ATICI <yahyasamilatici@gmail.com>
2023-10-19 19:45:36 +0300

    JS SAAT ODEVI

---
+++ javascriptclock/js/clock.js
@@ -0,0 +1,17 @@
+let fName = prompt('Adınızı Giriniz')
+console.log(fName)
+
+let myName = document.getElementById('myName')
+myName.innerHTML = fName
+console.log(myName.innerHTML)
+
+let nDate = Date()
+
+let myClock = document.getElementById('myClock')
+myClock.innerHTML = nDate
+console.log(myClock.innerHTML)
+
+
+
+
+
